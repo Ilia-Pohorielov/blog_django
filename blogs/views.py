ba6fb31d325b42653f4d5b3755a0390d37cf8732
ilia.pohorielov <ilia.pohorielov@gmail.com>
2017-07-17 21:43:23 +0300

    added view and template for detailed blog

--- blogs/views.py
+++ blogs/views.py
@@ -1,11 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 # from django.utils import timezone
 from .models import Post
 # Create your views here.
+
 def post_list(request):
     posts = Post.objects.all()
     return render(
         request,
         'blogs/homepage.html',
-        {'posts':posts}
+        {'posts': posts}
+    )
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(
+        request,
+        'blogs/inner_blog.html',
+        {'post': post}
     )
