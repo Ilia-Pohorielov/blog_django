6ab7b33172dd735fa4b92832e4e19521db98bfad
ilia.pohorielov <ilia.pohorielov@gmail.com>
2017-07-17 23:14:15 +0300

    add form for create post

--- blogs/views.py
+++ blogs/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 # from django.utils import timezone
 from .models import Post
+from .forms import PostForm
 # Create your views here.
 
 def post_list(request):
@@ -10,6 +11,15 @@ def post_list(request):
         'blogs/homepage.html',
         {'posts': posts}
     )
+
+def post_new(request):
+    form = PostForm()
+    return render(
+        request,
+        'blogs/blog_add.html',
+        {'form': form}
+    )
+
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
